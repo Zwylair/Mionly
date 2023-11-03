01ed41438b5a439289dc95731554b6bdb2c59a29
Zwylair <82669735+Zwylair@users.noreply.github.com>
2023-11-03 23:09:35 +0200

    Update base.js

--- web/js/base.js
+++ web/js/base.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     // trying to restrict opening devtools & breaking the logics of test loading & checking
 
+    // prevent opening the context menu
     document.addEventListener('contextmenu', e => { e.preventDefault(); });
-    document.addEventListener('keydown', e => { e.preventDefault(); });
+
+    // allow single key presses (letters, numbers, symbols), block any other key combinations
+    document.addEventListener('keydown', function(e) {
+        if (e.key && !(e.ctrlKey || e.altKey || e.shiftKey || e.metaKey || e.key.startsWith("F"))) {} else { e.preventDefault(); }
+    });
 });
 
 // a simple def against those who know what base64 is, but don't dare to get into the code 🤗
